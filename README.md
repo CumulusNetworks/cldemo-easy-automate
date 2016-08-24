b2aaeaa54c856d742dc2c556ae48814d520e9bae
Barry Peddycord III <ishara@isharacomix.org>
2016-08-24 11:28:31 -0400

    programming

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# cldemo-easy-automate
-Prototype
+
+
+
+
+### Demo
+
+# On github, create a fork of https://github.com/cumulusnetworks/easyautomate
+
+
+git clone https://github.com/MYFORK/easyautomate
+python collect.py leaf01,leaf02,spine01,spine02
---
+++ collect.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+
+import sys
+import os
+import paramiko
+
+
+def run(ssh, cmd):
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    stdout.channel.recv_exit_status()
+    return stdout.read()
+
+
+if __name__ == '__main__':
+    devices = sys.argv[1].split(',') if len(sys.argv) >= 2 else []
+    username = sys.argv[2] if len(sys.argv) >= 3 else 'cumulus'
+    password = sys.argv[3] if len(sys.argv) >= 4 else 'CumulusLinux!'
+
+    # Create directories
+    for device in devices:
+        try:
+            os.makedirs('roles/%s/network'%device)
+            os.makedirs('roles/%s/quagga'%device)
+            os.makedirs('roles/%s/cumulus'%device)
+        except os.error:
+            pass
+
+    # Log into each device and collect network and quagga information
+    for device in devices:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(device, username=username, password=password)
+
+        # Figure out all the files we're gonna hit
+        files = "/etc/network:\n"
+        files += run(ssh, 'ls -R1 /etc/network')
+        files += "\n"
+        files += "/etc/quagga:\n"
+        files += run(ssh, 'ls -R1 /etc/quagga')
+        files += "\n"
+        files += "/etc/cumulus:\n"
+        files += run(ssh, 'ls -R1 /etc/cumulus')
+        files += "\n"
+
+        current_dir = ''
+        for l in files.splitlines():
+            l = l.strip()
+            if l.endswith(':'):
+                current_dir = l.split(':')[0]
+            elif l and not l.endswith('/'):
+                content = run(ssh, "cat %s/%s"%(current_dir, l))
+                try:
+                    os.makedirs('roles/%s/%s'%(device, current_dir.split('etc', 1)[-1]))
+                except os.error:
+                    pass
+                with file('roles/%s/%s/%s'%(device, current_dir.split('etc', 1)[-1], l), 'w') as f:
+                    f.write(content)
